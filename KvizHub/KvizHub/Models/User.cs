2185770d014f3267a1fb76b800ea56dfcd4b1bf4
12nikola <102314632+12nikola@users.noreply.github.com>
2025-09-07 20:34:31 +0200

    s

---
+++ KvizHub/KvizHub/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace KvizHub.Models
+{
+    public class User
+    {
+        public string KorisnickoIme { get; set; }
+        public string Email { get; set; }   
+        public string Password { get; set; }
+        public string Image { get; set; }
+    }
+}
