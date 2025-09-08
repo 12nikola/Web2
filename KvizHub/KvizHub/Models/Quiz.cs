54cd738791c79d9ca97c296ee5be4d0d2d95cb2b
12nikola <102314632+12nikola@users.noreply.github.com>
2025-09-08 21:32:57 +0200

    sa

---
+++ KvizHub/KvizHub/Models/Quiz.cs
@@ -0,0 +1,16 @@
+﻿namespace KvizHub.Models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public int TimeLimit { get; set; } 
+        public string Difficulty { get; set; }
+        public string CreatorUsername { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
